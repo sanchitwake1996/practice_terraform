16363d6f291209a3827b428a302ca7af7b54c4b1
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-08-18 19:36:00 +0530

    add terraform file

--- Day2/main.tf
+++ Day2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
     ami = "ami-06e11c4cc68c362dd"
     instance_type = "t2.micro"
-    vpc_security_group_ids = []
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
     tags = {
       env = "dev"
     }
